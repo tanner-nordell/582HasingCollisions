57af3c10094ae34ee7eb7ea17cd7206976065ca1
tanner-nordell <hnordell@seas.upenn.edu>
2022-09-10 22:56:57 -0400

    first commit

--- hash_collision.py
+++ hash_collision.py
@@ -9,32 +9,28 @@ def hash_collision(k):
         print( "Specify a positive number of bits" )
         return( b'\x00',b'\x00' )
 
-    mask = 2**k - 1
-
+    mask = (1<<k)-1
 
     size = 16
     x = os.urandom(size)
-    print("str(x)" + str(x))
-    print("str(x) in hex "+ str(x.hex()))
+    print("x in binary: ", x)
+    print("x in hex: " + x.hex())
     x_hashed = hashlib.sha256(x)
-    #print("x_hashed:  " + x_hashed)
-    x_last_k_bits = int(x_hashed.hexdigest(),16) & mask
-    while(1): #fix this
+    x_last_k_bits = int(x_hashed.hexdigest(), 16) & mask
+    print("h(x) = ", x_hashed.hexdigest(), "  h(x) last", k ,"bits in hex:", hex(x_last_k_bits))
+    #print("h(x) last", k ,"bits in hex:", hex(x_last_k_bits))
+
+    while(1):
         y = os.urandom(size) #create a string of random bytes
         y_hashed = hashlib.sha256(y)
-        y_last_k_bits = int(y_hashed.hexdigest(),16) & mask
-
+        y_last_k_bits = int(y_hashed.hexdigest(), 16) & mask
 
         #check to see if last k bits match
         if(x_last_k_bits == y_last_k_bits):
             break
-
-    #Collision finding code goes here
-
-
-
-    x = b'\x00'
-    y = b'\x00'
-    
+    print("y in binary: ", y)
+    print("y in hex: " + y.hex())
+    print("h(y) = ", y_hashed.hexdigest(), "  h(y) last", k ,"bits in hex: ", hex(y_last_k_bits))
+    #print("h(y) last bits in hex: ", hex(y_last_k_bits))
     return( x, y )
 
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from hash_collision import hash_collision
 
 if __name__ == '__main__':
-   print("(x,y) = " + str(hash_collision(2)))
+   result = str(hash_collision(16))
+   print(" ")
+   print("(x,y) = ", result)
 
 
